e02f78f6de8956476c8d658080b2fa0512a7c68b
Gustavo <gustavorocha8971p@gmail.com>
2025-01-04 09:30:04 -0300

    Fazendo alterações no código fonte

---
+++ src/main/java/com/AppRH/AppRH/Start/AppRHApplication.java
@@ -0,0 +1,14 @@
+package com.AppRH.AppRH.Start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRHApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRHApplication.class, args);
+    }
+
+}
+
